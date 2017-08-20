ca9574a51bfce51f896f1e45338d468b63863a7d
haiphi <ahodges230@gmail.com>
2017-08-20 14:44:32 -0500

    Included sql table

---
+++ database.sql
@@ -0,0 +1,8 @@
+CREATE TABLE employees (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(40) NOT NULL,
+    last_name VARCHAR(40) NOT NULL,
+    job_title VARCHAR(40) NOT NULL,
+    employees_salary FLOAT NOT NULL
+);
+
